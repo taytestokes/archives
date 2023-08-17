bcacbe5d47a6fbcf255de5fccf0173768948cbf7
taytestokes <taytestokes9328@gmail.com>
2023-08-17 13:11:00 -0600

    Remove tap tests - will revisit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,75 @@
 import "dotenv/config";
 
-import buildServer from "./server";
+import Fastify, {
+  FastifyInstance,
+  FastifyRequest,
+  FastifyReply,
+} from "fastify";
+import fastifyJwt, { JWT } from "@fastify/jwt";
 
-const server = buildServer();
+import { healthRoutes } from "./routes/v1/health";
+import { userRoutes } from "./routes/v1/user";
+import { recordRoutes } from "./routes/v1/record";
+
+import { userSchemas } from "./schemas/user";
+import { recordSchemas } from "./schemas/record";
+
+declare module "@fastify/jwt" {
+  export interface FastifyJWT {
+    user: {
+      id: string;
+      email: string;
+    };
+  }
+}
+
+declare module "fastify" {
+  interface FastifyRequest {
+    jwt: JWT;
+  }
+
+  export interface FastifyInstance {
+    authenticate: any;
+  }
+}
+
+// Server Instance
+const server: FastifyInstance = Fastify();
+
+// Plugins
+server.register(fastifyJwt, {
+  secret: process.env.SECRET || "",
+});
+
+// Decorators
+server.decorate(
+  "authenticate",
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await request.jwtVerify();
+    } catch (error) {
+      reply.send(error);
+    }
+  }
+);
+
+// Hooks
+server.addHook("preHandler", (request, reply, next) => {
+  request.jwt = server.jwt;
+  return next();
+});
+
+// Routes
+server.register(healthRoutes, { prefix: "api/v1/health" });
+server.register(userRoutes, { prefix: "api/v1/users" });
+server.register(recordRoutes, { prefix: "api/v1/records" });
+
+// Schemas
+const schemas = [...userSchemas, ...recordSchemas];
+
+for (const schema of schemas) {
+  server.addSchema(schema);
+}
 
 server.listen({ port: Number(process.env.PORT), host: "0.0.0.0" }, (error) => {
   if (error) {
